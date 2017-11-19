62bcadd5d2f29da13e52a1f5689b67e09a8be11c
Karteek2002 <karthikvsgoteti@gmail.com>
2017-11-19 12:20:59 +0530

    ki

---
+++ Test2/src/Test2Packege/Test2sample.java
@@ -0,0 +1,10 @@
+package Test2Packege;
+
+public class Test2sample {
+
+	public static void main(String[] args) {System.out.println("Hello Friends");
+		// TODO Auto-generated method stub
+
+	}
+
+}
